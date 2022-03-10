9950d212a3a602f0beddd2991e04feded87b4197
Jeff Mader <jmader@vm-koaserver2.(none)>
2022-03-10 13:17:24 -1000

    added case for DEIMOS lev2 by datadir

--- src/ingest_api/ingest_api_lev2.py
+++ src/ingest_api/ingest_api_lev2.py
@@ -1,3 +1,4 @@
+from os import listdir
 from os.path import isdir
 from datetime import datetime as dt
 from ingest_api.ingest_api_common import *
@@ -34,6 +35,8 @@ def update_lev2_parameters(parsedParams, reingest, config, conn, dbUser='koa_tes
 
     # If KOAID exists, then this will do the same as level 1 calls, use that function
     if koaid != None:
+        # This data is processed as a whole, add a WAITING database entry and
+        # wait for the final call
         return update_lev1_parameters(parsedParams, reingest, config, conn, dbUser, 'WAITING')
 
     # metrics will exist for calls from IPAC
@@ -61,6 +64,39 @@ def update_lev2_parameters(parsedParams, reingest, config, conn, dbUser='koa_tes
                     _, parsedParams = update_db_data(parsedParams, config, conn, dbUser)
     # DRP completion
     else:
+        # For DEIMOS we will get a datadir with multiple KOAIDs
+        # Need to add a database entry for each
+        # This may work for other instruments with slitmasks as well
+        if instrument == 'DEIMOS':
+            # Check datadir
+            processDir = 'error'
+            if 'datadir' in parsedParams.keys():
+                processDir = parsedParams['datadir']
+            if not isdir(processDir):
+                parsedParams['apiStatus'] = 'ERROR'
+                parsedParams['ingestErrors'].append(f'datadir ({processDir}) does not exist')
+            else:
+                # Get list of KOAIDs and add to queue
+                koaidList = [f for f in listdir(f"{processDir}/Science") if f.endswith('.fits')]
+                found = []
+                for koaid in koaidList:
+                    for k in koaid.split('_'):
+                        if k.startswith('DE.'):
+                            parsedParams['koaid'] = k[:20]
+                            if k[:20] not in found:
+                                lev1Params = update_lev1_parameters(parsedParams, reingest, config, conn, dbUser, 'QUEUED')
+                                if lev1Params['apiStatus'] == 'COMPLETE':
+                                    found.append(k[:20])
+                # Construct message with each koaid
+                if len(found) > 0:
+                    parsedParams['koaid'] = ''
+                    msg = ''
+                    for k in found:
+                        msg = f'{msg} {k}'
+                    parsedParams['statusMessage'] = f'{msg.strip()} added to DRP archiving queue'
+
+            return parsedParams
+
         # Change all WAITING koa_status entries to QUEUED
         query = "update koa_status set status='QUEUED'"
         query = f"{query} where instrument='{instrument}' and level=2"
